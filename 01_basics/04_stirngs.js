229137fe019fa50c4c4a57e76c4a19a8bac010b2
Alok Rawat <alokrawat2121@gmail.com>
2023-11-01 07:19:26 +0530

    data type summary and string basic

---
+++ 01_basics/04_stirngs.js
@@ -0,0 +1,61 @@
+const name = "Alok";
+const repoCount = 90;
+
+//console.log(`Hi, I am ${name} and my repo count ${repoCount} along with I am Android developer`);
+//Hi, I am Alok and my repo count 90 along with I am Android developer
+//here in the ${} symbol we can use come methods as toupper case and lower and etc
+
+//lets jump to the interesting topic details of the string
+
+const gameName = new String("Alok Rawat");
+//return normal string but it is also behave like an object let's dive in...
+//if we go to the console.log in browser section we can see the following section..
+// >>String {'Alok Rawat'}
+//0: "A"
+//1: "l"
+//2: "o"
+//3: "k"
+//4: " "
+//5: "R"
+//6: "a"
+//7: "w"
+//8: "a"
+//9: "t"
+//length: 10
+
+//console.log(gameName.__proto__);//{}
+
+//console.log(gameName.toUpperCase());//ALOK RAWAT , here the actual value of the gameName is not change because it primitive data types
+//console.log(gameName);//[String: 'Alok Rawat']
+
+const newString = gameName.substring(0, 4);
+//console.log(newString);//Alok , here 4th character is not included....
+//console.log(newString.length);//4
+
+
+const anotherString = gameName.slice(-8, 5);//here three character ok is 2 and another which is whitespace also in the slice we can give negative values in the end and start 
+//console.log(anotherString);//ok
+//console.log(anotherString.length);//3
+
+
+//trim method is used to Removes the leading and trailing white space and line terminator characters from a string.
+const userThere = "         Stark in company          "
+//console.log(userThere);//         Stark in company          
+//console.log(userThere.trim());//Stark in company
+
+const url = "alokrawat2121@gamil.com%20tony"//browser convert the space into url encoding
+//console.log(url.replace('%20' , '-'));//alokrawat2121@gamil.com-tony
+//console.log(url.includes('alok'));//true
+
+
+const str = 'The quick brown fox jumps over the lazy dog.';
+
+const words = str.split(' ');
+console.log(words);//used for making the string look like array
+//[
+//     'The',   'quick',
+//     'brown', 'fox',
+//     'jumps', 'over',
+//     'the',   'lazy',
+//     'dog.'
+//   ]
--- 01_basics/datatypes_summary.js
+++ 01_basics/datatypes_summary.js
@@ -5,12 +5,19 @@
 //on the bases of storing and retrieving data in the memroy it (data) is categorised into two parts primitive and non-primitive
 
 
-//Primitive 
+//Primitive (call by values)
 
 //ye call by reference hote hain means jab bhi hum enhe call krte hain to ye hume copy ke form mein milte hain and jo bhi hum changes krte hain wo copy mein hi milte hain na ki acutal waley mein...
 
 // 7 types : String , Number , Boolean , null , undefined ,Symbol ,BigInt
 
+
+// datatypes of the data is given below:
+// undefined = "undefined"
+// number = "number"
+// null = "object" why ? because it have unique propert and that's why it should be null
+//Symbol = "Symbol"
+//bigint = "bigint"
 
 //Symbol data types are used to provide unique identifiers to same types or to different types depending on the need of the situation
 
@@ -18,9 +25,70 @@
 const id = Symbol('123')
 const anotherId = Symbol('123')
 
-console.log(id === anotherId );
+// console.log(id === anotherId );
 //return false
 
-// Reference Type (Non Primitive)
+// console.log(id , 'id');//Symbol(123)
+// console.log(anotherId , 'another id');//Symbol(123)
+
+//BigInt data types is used for large number storage to make this number bigInt we should simply put "n" in the last of the number
+const bigNumber = 789946235956216163n;
+
+//console.log(typeof(bigNumber));//bigint
+ 
+
+
+
+
+// Reference Type (Non Primitive or call by refrence)
 
 // Array , Objects , Functions
+
+//all the reference type data ka data types jo hai wo function hi hota hai let check out
+
+const array = ['Apple' , "fruits" , "rose"];
+//console.log(typeof(array));//object
+
+const object = {
+    name: 'Alok',
+    age: 22
+}
+//console.log(typeof(object));//object
+
+const printMyNameObject = () => {
+    return object;
+}
+
+//console.log(typeof(printMyNameObject()));//object
+
+
+
+
+//***************************************Memory types in JavaScript ******************************************************** */
+
+//Stack and Heap
+// Stack memory is used in the primitive data types here we used stack because it would generate new copy of the same data not actual
+//Heap memory is used in the non-primitive here we used heap memory because we have to maintain the value of the data actual values not copy
+
+//example for stack
+
+let userName = "Alok Rawat"
+
+let guestName = userName;
+guestName = "Tony Stark";
+
+// console.log(userName );//Alok Rawat
+// console.log(guestName );//Tony Stark
+
+//example for heap
+
+let userOne = {
+    name: "Alok ",
+    email: "alok@google.com"
+}
+
+let userTwo = userOne;
+userTwo.email= "tony@google.com";
+
+//console.log(userOne?.email);//tony@google.com
+//console.log(userTwo?.email);//tony@google.com
